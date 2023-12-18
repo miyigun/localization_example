0d3bc413712bbf6d9c57acaff72f622a8fec30e1
Murat Iyigun <miyigun@hotmail.com>
2023-12-18 22:02:41 +0300

    Initial commit

---
+++ lib/models/dummy_students.dart
@@ -0,0 +1,21 @@
+
+
+import 'package:localization_example/models/student.dart';
+
+const dummyStudents = [
+  Student(
+    's1',
+    'John',
+    'IX',
+  ),
+  Student(
+    's2',
+    'Json',
+    'X',
+  ),
+  Student(
+    's3',
+    'Mary',
+    'XI',
+  ),
+];
